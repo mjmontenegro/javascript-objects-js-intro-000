db6acc6eb67741497be313a4bcab709407a520d9
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-12 05:24:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ let updatePlaylist = (playlist, artistName, songTitle) => {
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
